4dcb8d34b5b8e7a86ae5ed715727d182d07eefb9
gabriel <gabrielcomputacao@outlook.com>
2023-11-07 07:06:23 -0300

    feat: configuring button at storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,16 +1,31 @@
 import { styled } from "./styles";
+import { ComponentProps } from "react";
 
-const Button = styled("button", {
+export const Button = styled("button", {
   fontFamily: "$default",
   backgroundColor: "$ignite500",
   borderRadius: "$md",
-  padding: "$4",
+  padding: "$1 $4",
+  border: 0,
+  color: "$white",
+  fontWeight: "$bold",
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: "$2 $4",
+      },
+      big: {
+        fontSize: 16,
+        padding: "$3 $6",
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: "small",
+  },
 });
 
-export function App() {
-  return (
-    <>
-      <Button> Hello World </Button>
-    </>
-  );
-}
+export type ButtonProps = ComponentProps<typeof Button>;
