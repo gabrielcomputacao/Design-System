b20f21fd936de9adc62244cef041a3fd97744749
gabriel <gabrielcomputacao@outlook.com>
2023-11-11 06:45:43 -0300

    feat: creating Avatar react component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from "@storybook/react";
+import { Avatar, AvatarProps } from "@ignite-ui/react/src/index";
+
+export default {
+  title: "Data/Display",
+  component: Avatar,
+
+  args: {
+    src: "https:://github.com/gabrielcomputacao.png",
+    alt: "gabriel henrique",
+  },
+} as Meta<AvatarProps>;
+
+export const Primary: StoryObj<AvatarProps> = {};
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+};
